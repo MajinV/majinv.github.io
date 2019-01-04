b3866779fd8ac8a02f0056bec5d819d68d86c4f8
MajinV <robrainey@gmail.com>
2019-01-04 22:04:39 +0000

    bug fixes from jo's testing

--- scripts/shared.js
+++ scripts/shared.js
@@ -15,25 +15,3 @@ mobileNavButtonClose.addEventListener('click', function(){
     mobileNavMenu.classList.add('display');
 });
 
-const cats = [
-    "images/decorations/eternal-posy-centrepiece-1.jpg",
-    "images/decorations/anenomy-water-blur.jpg",
-    "images/decorations/eternal-posy-centrepiece-2.jpg"
-    ]
-    
-    const heroContainer = document.getElementById("hero-container");
-    
-    const cycleImages = (images, container, step) => {
-        images.forEach((image, index) => (
-        setTimeout(() => {
-            container.style.background = `linear-gradient(
-                0deg,
-                rgba(108, 108, 108, 0.7),
-                rgba(108, 108, 108, 0.7)
-              ),url(${image}) bottom 50% left 40% / cover no-repeat border-box`
-        }, step * (index + 1))
-    ))
-    setTimeout(() => cycleImages(images, container, step), step * images.length)
-    }
-    
-    cycleImages(cats, heroContainer, 10000)
