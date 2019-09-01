3288e6597fe2c317b3ec0f236cc133c05500b016
MajinV <robrainey@gmail.com>
2019-09-01 12:34:34 +0100

    proto lightbox for decos

--- scripts/shared.js
+++ scripts/shared.js
@@ -25,46 +25,99 @@ mobileNavButtonClose.addEventListener('click', function(){
 // })
 
 
-// Open the Modal
-function openModal() {
-    document.getElementById('modal').style.display = "flex";
-  }
+// // Open the Modal
+// function openModal() {
+//     document.getElementById('modal').style.display = "flex";
+//   }
   
-  // Close the Modal
-  function closeModal() {
-    document.getElementById('modal').style.display = "none";
-  }
+//   // Close the Modal
+//   function closeModal() {
+//     document.getElementById('modal').style.display = "none";
+//   }
   
-  var slideIndex = 1;
-  showSlides(slideIndex);
+//   var slideIndex = 1;
+//   showSlides(slideIndex);
   
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+//   // Next/previous controls
+//   function plusSlides(n) {
+//     showSlides(slideIndex += n);
+//   }
   
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+//   // Thumbnail image controls
+//   function currentSlide(n) {
+//     showSlides(slideIndex = n);
+//   }
   
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("gallery-modal__container");
-    //var dots = document.getElementsByClassName("demo");
-    let captionText = document.getElementById("gallery-modal__info-desc");
-    console.log(captionText);
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    //for (i = 0; i < dots.length; i++) {
-    //  dots[i].className = dots[i].className.replace(" active", "");
-    //}
-    captionText.innerHTML = slides[slideIndex-1].alt;
-    slides[slideIndex-1].style.display = "flex";
-    //dots[slideIndex-1].className += " active";
+//   function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("gallery-modal__container");
+//     //var dots = document.getElementsByClassName("demo");
+//     let captionText = document.getElementById("gallery-modal__info-desc");
+//     console.log(captionText);
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     //for (i = 0; i < dots.length; i++) {
+//     //  dots[i].className = dots[i].className.replace(" active", "");
+//     //}
+//     captionText.innerHTML = slides[slideIndex-1].alt;
+//     slides[slideIndex-1].style.display = "flex";
+//     //dots[slideIndex-1].className += " active";
 
     
-  }
+//   }
+
+  // Initialize here and run showSlide() to give your lightbox a default state.
+
+let slideIndex = 1;
+showSlide(slideIndex);
+
+// You are providing the functionality for your clickable content, which is 
+// your previews, dots, controls and the close button.
+
+function openLightbox() {
+  document.getElementById('Lightbox').style.display = 'block';
+}
+
+function closeLightbox() {
+  document.getElementById('Lightbox').style.display = 'none';
+};
+
+// Note that you are assigning new values here to our slidIndex.
+
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+};
+
+function toSlide(n) {
+  showSlide(slideIndex = n);
+};
+
+// This is your logic for the light box. It will decide which slide to show 
+// and which dot is active.
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName('slide');
+  let modalPreviews = document.getElementsByClassName('modal-preview');
+
+  if (n > slides.length) {
+    slideIndex = 1;	
+  };
+  
+  if (n < 1) {
+    slideIndex = slides.length;
+  };
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  };
+  
+  for (let i = 0; i < modalPreviews.length; i++) {
+    modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
+  };
+  
+  slides[slideIndex - 1].style.display = 'block';
+  modalPreviews[slideIndex - 1].className += ' active';
+};
