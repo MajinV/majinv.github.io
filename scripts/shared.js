e01752270af39e7024fe83512ad9dbae55bc9ee6
Rob Rainey <rrainey@groupcall.com>
2020-08-15 14:52:00 +0100

    implement lightbox and updated grid gallery

--- scripts/shared.js
+++ scripts/shared.js
@@ -12,3 +12,41 @@ mobileNavButtonClose.addEventListener('click', function(){
     mobileNavMenu.classList.add('display');
 });
 
+document.addEventListener('keydown', (e) => {
+    
+    switch (e.key) {
+            
+        case 'Escape':
+        case 'Esc':
+            window.location.hash = "#close";
+            break;
+            
+        case 'Left':
+        case 'ArrowLeft':
+            const currentEl = document.querySelector(window.location.hash);
+            if (!currentEl) return;
+
+            const prevEl = currentEl.previousElementSibling;
+            if (!prevEl) return;
+
+            window.location.hash = `#${prevEl.id}`;
+            
+            break;
+            
+        case 'Right':
+        case 'ArrowRight':
+            const currentEl2 = document.querySelector(window.location.hash);
+            if (!currentEl2) return;
+
+            const nextEl = currentEl2.nextElementSibling;
+            if (!nextEl) return;
+
+            window.location.hash = `#${nextEl.id}`;
+            
+            break;
+            
+        default:
+            return;
+            
+    }
+});
